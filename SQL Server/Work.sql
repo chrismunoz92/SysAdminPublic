ac7c591a081737c79a946a19197cc804bc1ea667
Christopher Munoz <chris.munoz@PC-LM31370.overstock.com>
2020-04-08 17:19:37 -0600

    SQL Server

---
+++ SQL Server/Work.sql
@@ -0,0 +1,21 @@
+-- IT SECURITY SOFTWARE REPORT --
+SELECT cl.CollectionName, cm.Name
+FROM collectionmembers cm, collections cl 
+WHERE cl.SiteID = cm.SiteID AND cl.CollectionName = 'Workstations Without Carbon Black'
+OR cl.SiteID = cm.SiteID AND cl.CollectionName = 'Workstations Without CB Protection Agent (Bit9)'
+OR cl.SiteID = cm.SiteID AND cl.CollectionName = 'Workstations Without Symantec'
+ORDER BY cl.CollectionName ASC
+
+-- COMPUTERS WITH INTEL(R) HD GRAPHICS 620 - VIDEO CARD --
+SELECT S.Name0 'Hostname', S.User_Name0 'Last Logon User', S.Last_Logon_Timestamp0 'Last Logon Time'
+FROM v_R_System S, collectionmembers CM, collections CL
+WHERE CM.Name = S.Name0 AND CM.SiteID = CL.SiteID AND CL.CollectionName = 'Workstations With Intel HD Graphics 620 - Video Card'
+ORDER BY S.Name0 ASC
+
+-- Old Symantec Report --
+SELECT collectionmembers.Name
+FROM collectionmembers
+WHERE SiteID = 'SL40021D'
+ORDER BY Name ASC
+
+--TEST----------------

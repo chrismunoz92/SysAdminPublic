1edd6f80f61df429ba5923652b4fa700bd37136b
Christopher Munoz <chris.munoz@PC-LM31370.overstock.com>
2020-04-16 13:41:18 -0600

    Get-RegistryKeyPath.ps1
    Created a script to get the Registy Key Path of a Windows OS Computer. Can be altered to get further properties like DisplayName or UninstallString. If you grab the UninstallString property, then you can convert to a batch file for uninstalling.

---
+++ Powershell/Get-RegistryKeyPath.ps1
@@ -0,0 +1,40 @@
+﻿<#   
+.SYNOPSIS   
+Script that returns the Registy Key Path for the Software Name you pass as a parameter.
+     
+.PARAMETER Software
+The Software whose Registry Key Path you need.
+
+.NOTES   
+Name: Get-RegistyKey.ps1
+Author: Christopher Munoz
+DateCreated: 04/16/2020
+
+.EXAMPLE
+	.\Get-RegistryKey.ps1 -Software Symantec
+#>
+
+
+param(
+    [string]$Software
+)
+
+$Software = "Symantec"
+
+$PATHS = @("HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall",
+               "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+
+
+ForEach ($path in $PATHS) {
+    $installed = Get-ChildItem -Path $path |
+        ForEach { Get-ItemProperty $_.PSPath } |
+        Where-Object { $_.DisplayName -match $Software } |
+        Select-Object -Property PSPath
+
+    ForEach ($app in $installed) {
+        $Key = "$($app.PSPath)"
+    }
+}
+
+$Key = $Key.Split("::")
+$Key
--- SQL Server/Work.sql
+++ SQL Server/Work.sql
@@ -6,10 +6,11 @@ OR cl.SiteID = cm.SiteID AND cl.CollectionName = 'Workstations Without CB Protec
 OR cl.SiteID = cm.SiteID AND cl.CollectionName = 'Workstations Without Symantec'
 ORDER BY cl.CollectionName ASC
 
--- COMPUTERS WITH INTEL(R) HD GRAPHICS 620 - VIDEO CARD --
-SELECT S.Name0 'Hostname', S.User_Name0 'Last Logon User', S.Last_Logon_Timestamp0 'Last Logon Time'
-FROM v_R_System S, collectionmembers CM, collections CL
-WHERE CM.Name = S.Name0 AND CM.SiteID = CL.SiteID AND CL.CollectionName = 'Workstations With Intel HD Graphics 620 - Video Card'
+-- COMPUTERS WITH INTEL(R) HD GRAPHICS 620 - VIDEO CARD REPORT --
+SELECT S.Name0 'Hostname', CS.Model0 'Model', S.User_Name0 'Last Logon User', S.Last_Logon_Timestamp0 'Last Logon Time'
+FROM v_R_System S, collectionmembers CM, collections CL, v_GS_computer_system CS
+WHERE CM.Name = S.Name0 AND CM.Name = CS.Name0 AND CM.SiteID = CL.SiteID 
+AND CL.CollectionName = 'Workstations With Intel HD Graphics 620 - Video Card'
 ORDER BY S.Name0 ASC
 
 -- Old Symantec Report --
@@ -18,4 +19,10 @@ FROM collectionmembers
 WHERE SiteID = 'SL40021D'
 ORDER BY Name ASC
 
---TEST----------------
+-- TEST --
+SELECT S.Name0 'Hostname', CS.Model0 'Model', S.User_Name0 'Last Logon User', S.Last_Logon_Timestamp0 'Last Logon Time'
+FROM v_R_System S, collectionmembers CM, collections CL, v_GS_computer_system CS
+WHERE CM.Name = S.Name0 AND CM.Name = CS.Name0
+AND CM.SiteID = CL.SiteID 
+AND CL.CollectionName = 'Workstations With Intel HD Graphics 620 - Video Card'
+ORDER BY S.Name0 ASC
